8594c500ba869ac808f467b8a5c5a14cc105d1bc
Julian <jperelli@gmail.com>
2016-02-12 00:59:46 -0300

    changing some contact info

--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup, find_packages
 version = __import__('class_based_auth_views').__version__
 
 setup(
-    name="django-class-based-auth-views",
+    name="django-class-based-auth-views-jp",
     version=version,
-    url='http://github.com/stefanfoulis/django-class-based-auth-views',
+    url='http://github.com/jperelli/django-class-based-auth-views',
     license='BSD',
     platforms=['OS Independent'],
     description="A reimplementation of django.contrib.auth.views as class based views.",
     long_description=open('README.rst').read(),
     author='Stefan Foulis',
     author_email='stefan@foulis.ch',
-    maintainer='Stefan Foulis',
-    maintainer_email='stefan@foulis.ch',
+    maintainer='Julian Perelli',
+    maintainer_email='jperelli@gmail.com',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
